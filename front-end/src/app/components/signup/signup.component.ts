789e6445b926cd4781a76ddbc047fe1763887859
jasonfritsche <fritsche.jason@gmail.com>
2021-11-14 15:28:05 -0600

    signup form layout initial

--- front-end/src/app/components/signup/signup.component.ts
+++ front-end/src/app/components/signup/signup.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder) {}
 
-  constructor() { }
+  public signupForm = this.formBuilder.group({
+    firstName: [''],
+    lastName: [''],
+    workEmail: [''],
+    password: [''],
+    phone: [''],
+  });
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
